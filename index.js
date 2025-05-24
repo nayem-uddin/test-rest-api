0207f7f861b277d50e3483a8a22e91ea9ccfc762
nayem-uddin <mnubpial@gmail.com>
2025-05-24 16:14:44 +0600

    test commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const fs = require("fs-extra");
+const port = process.env.port || 8080;
+const usersListDirectory = `${__dirname}/users.json`;
+
+const server = app.listen(port, () => {
+  console.log("App running at http://localhost:" + port);
+});
+
+// get the whole users' list
+app.get("/", (req, res) => {
+  fs.readFile(usersListDirectory, "utf-8", (err, data) => {
+    res.end(data);
+  });
+});
+
+// get a specific user's details
+app.get("/:id", (req, res, next) => {
+  fs.readFile(usersListDirectory, "utf-8", (err, data) => {
+    try {
+      const users = JSON.parse(data);
+      const userIndex = `user${req.params.id}`;
+      if (!(userIndex in users)) {
+        throw new Error("User doesn't exist");
+      }
+      const user = users[userIndex];
+      res.end(JSON.stringify(user));
+    } catch (error) {
+      next(error.message);
+    }
+  });
+});
